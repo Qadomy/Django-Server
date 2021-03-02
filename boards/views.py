29e3e555c301ed02fc211134cef2773509b558b9
Ali Qadomy <ali.qadomy@hotmail.com>
2021-03-02 02:15:32 +0200

    ..

--- boards/views.py
+++ boards/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, \n"
+    return HttpResponse("Hello,"
                         "I'm Ali Qadomy, I'm Mobile Application Developer")
