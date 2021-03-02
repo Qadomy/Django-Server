2e75f8f39bd4641073c25af56b50cee6448ddeda
Qadomy <ali.qadomy@hotmail.com>
2021-03-02 06:13:33 +0200

    set database from postgres into project

--- dicussion_board/settings.py
+++ dicussion_board/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'dicussion_board.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Boards_django',
+        'USER': 'postgres',
+        'PASSWORD': '124726',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
