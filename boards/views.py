31d2557f05e4f824007dd1775b0378686ecbd43c
Qadomy <ali.qadomy@hotmail.com>
2021-03-03 14:33:43 +0200

    add view data from database and display in web page

--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 
+from .models import Board
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello,"
-                        "I'm Ali Qadomy, I'm Mobile Application Developer")
+    boards = Board.objects.all()
+    boards_names = []
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = "<br>".join(boards_names)
+    return HttpResponse(response_html)
