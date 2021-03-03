40d017009de3dfab3fd2186121cd3f47111cc0ea
Qadomy <ali.qadomy@hotmail.com>
2021-03-03 14:53:37 +0200

    display data in html file in web

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Board
 
@@ -6,9 +6,4 @@ from .models import Board
 # Create your views here.
 def home(request):
     boards = Board.objects.all()
-    boards_names = []
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = "<br>".join(boards_names)
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards': boards})
