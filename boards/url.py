9da8faa498d2d1d4a5d5fd116b2aa1bb28fbddbf
Qadomy <ali.qadomy@hotmail.com>
2021-03-04 18:35:58 +0200

    create "new topics page" and display it in topics page

--- boards/url.py
+++ boards/url.py
@@ -4,5 +4,6 @@ from boards import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('boards/<int:board_id>/', views.board_topics, name='board_topics')
+    path('boards/<int:board_id>/', views.board_topics, name='board_topics'),
+    path('boards/<int:board_id>/new', views.new_topic, name='new_topic')
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Board
+from .models import Board, Topic, Post
 
 
 # Create your views here.
@@ -12,3 +13,25 @@ def home(request):
 def board_topics(request, board_id):
     board = get_object_or_404(Board, pk=board_id)
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, board_id):
+    board = get_object_or_404(Board, pk=board_id)
+    # here we get data from form
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+        user = User.objects.first()
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            created_by=user
+        )
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+        # back to page
+        return redirect('board_topics', board_id=board.pk)
+    return render(request, 'new_topic.html', {'board': board})
