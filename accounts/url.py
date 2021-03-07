c9d9b17055f77a5d77ef335ca6a6959e813c3643
Qadomy <ali.qadomy@hotmail.com>
2021-03-07 23:22:09 +0200

    add login from to project

--- accounts/url.py
+++ accounts/url.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login')
 ]
