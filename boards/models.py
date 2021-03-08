18043365c825f2cb46a8e3af4661fd8ccc221af9
Qadomy <ali.qadomy@hotmail.com>
2021-03-08 17:49:04 +0200

    add posts and topics count, and last post

--- boards/models.py
+++ boards/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import Truncator
 
 
 class Board(models.Model):
@@ -9,6 +10,14 @@ class Board(models.Model):
     def __str__(self):
         return self.name
 
+    # function for return the count of posts
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    # function for return the last post
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_dt').first()
+
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
@@ -16,9 +25,16 @@ class Topic(models.Model):
     created_by = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
     created_dt = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.subject
+
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
     topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     created_dt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
