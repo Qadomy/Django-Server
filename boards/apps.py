8081e1a3f40f14495ed6c61c4e697aaf0665a5fd
Qadomy <ali.qadomy@hotmail.com>
2021-03-10 03:47:53 +0200

    add suit to admin panel

--- boards/apps.py
+++ boards/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from suit.apps import DjangoSuitConfig
 
 
 class BoardsConfig(AppConfig):
     name = 'boards'
+
+
+class SuitConfig(DjangoSuitConfig):
+    layout = 'vertical'
--- dicussion_board/settings.py
+++ dicussion_board/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'boards.apps.SuitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
